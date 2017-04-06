35fdab99623e035e59c0b2dcec4ba81e70070fab
CarpeSwag <james@garbagnati.com>
2017-04-06 14:15:06 -0700

    Set limits on the rotations and adjusted where the camera is pointed.

--- ProjectFiles/scripts/main.js
+++ ProjectFiles/scripts/main.js
@@ -8,10 +8,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		// This creates and positions a free camera (non-mesh)
 		var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 4,
-			Math.PI / 3, 10, new BABYLON.Vector3.Zero(), scene);
+			Math.PI / 3, 10, new BABYLON.Vector3(0,1,0), scene);
 
 		// This targets the camera to scene origin
 		camera.setTarget(BABYLON.Vector3.Zero());
+		
+		// Set camera view limits
+			camera.upperBetaLimit = Math.PI / 2;
+			camera.lowerRadiusLimit = 7.5;
 
 		// This attaches the camera to the canvas
 		camera.attachControl(canvas, true);
