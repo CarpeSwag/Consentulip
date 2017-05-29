ed4f060963e01a4db29f253255ed85d1d14a4f96
CarpeSwag <james@garbagnati.com>
2017-05-28 20:03:44 -0700

    Created a queue system to cycle in new timed messages.

--- ProjectFiles/scripts/talk.js
+++ ProjectFiles/scripts/talk.js
@@ -5,8 +5,11 @@ var Talk = {
 	// Pointer related
 	isPointerDown: false,
 	
-	// Text variable
-	contents: '',
+	// Message Queue
+	queue: [],
+	extraTime: 0,
+	active: false,
+	message: '',
 	
 	onLoad: function() {
 		this.text = document.getElementById('flower-name');
@@ -16,7 +19,71 @@ var Talk = {
 		this.text.innerHTML = txt;
 	},
 	
-	setDelayedText: function(txt, ms) {
-		setTimeout(function() {Talk.setText(txt);}, ms);
+	queueMessage: function(msg, timer, delay, keepMsg) {
+		// Set optional value
+		delay = delay || 0;
+		keepMsg = keepMsg === true;
+		
+		if (this.active) {
+			var len = this.queue.length;
+			var tail = (len == 0)? this.message:
+				this.queue[len - 1].message;
+			
+			// Check if this is this is sending the same message
+			// as the tail request
+			if (tail === message) {
+				if (len == 0) {
+					this.extraTime += timer;
+				} else {
+					this.queue[len - 1].time += timer;
+				}
+			} else {
+				// Add to tail of the queue
+				this.queue.push({
+					message: msg,
+					time: timer,
+					keep: keepMsg
+				});
+			}
+		} else {
+			// No message is currently displayed so start
+			this.active = true;
+			setTimeout(function() {
+				Talk.startMessageQueue(msg, timer);
+			}, delay);
+		}
+	},
+	
+	startMessageQueue: function(msg, timer) {
+		this.setText(msg);
+		setTimeout(this.rotateMessage, timer);
+	},
+	
+	rotateMessage: function() {
+		if (this.extraTime != 0) {
+			// This message gets another cycle
+			setTimeout(this.rotateMessage, this.extraTime);
+			this.extraTime = 0;
+		} else if (this.queue.length > 0) {
+			// Rotate the message
+			this.displayNext();
+		} else {
+			// Clear the messages
+			this.clearMessage();
+			active = false;
+		}
+	},
+	
+	displayNext: function() {
+		// Pop next item off queue and display that.
+		var next = this.queue.shift();
+		this.message = next.message;
+		this.setText(next.message);
+		setTimeout(this.rotateMessage, next.time);
+	},
+	
+	clearMessage: function() {
+		this.message = '';
+		this.setText('');
 	}
 };
